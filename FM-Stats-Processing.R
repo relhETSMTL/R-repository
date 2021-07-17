ce67e73eb4982d23d1807dd02711d150eb9280f8
Roberto E. Lopez-Herrejon <roberto.lopez@etsmtl.ca>
2021-07-17 11:19:46 -0400

    Loading the FM stats file

---
+++ FM-Stats-Processing.R
@@ -0,0 +1,7 @@
+# Processing Feature Model Statistics
+# Author Roberto E. Lopez-Herrejon roberto.lopez@etsmtl.ca
+# ETS Univeristy of Quebec
+
+# Loads the CVS file with the statistics of the 
+fmData <- read.csv(file = 'fmstats.csv')
+attach (fmData)
