4d300d968025c90463613f221108165273302528
Roberto E. Lopez-Herrejon <roberto.lopez@etsmtl.ca>
2021-07-18 13:34:28 -0400

    Boxplot with jitter for NoF.

--- FM-Stats-Processing.R
+++ FM-Stats-Processing.R
@@ -49,13 +49,52 @@ boxplot(fmData)
 boxstyle <- fmData %>% 
   ggplot(aes(y=NoF)) +
   geom_boxplot() +
-  geom_jitter() + 
   theme_minimal() +
   labs(y="Number of FMs")
 
 boxstyle
 
+###### Test with jitter
+library(viridis)
+
+# create a dataset
+data <- data.frame(
+  name=c( rep("A",500), rep("B",500), rep("B",500), rep("C",20), rep('D', 100)  ),
+  value=c( rnorm(500, 10, 5), rnorm(500, 13, 1), rnorm(500, 18, 1), rnorm(20, 25, 4), rnorm(100, 12, 1) )
+)
+
+# Plot
+data %>%
+  ggplot( aes(x=name, y=value, fill=name)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6) +
+  geom_jitter(color="black", size=0.4, alpha=0.9) +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("A boxplot with jitter") +
+  xlab("")
 
 
+## Reformatting the fmData to obtain a list of Values
+fmDataBox <- data.frame(
+  name=c(rep("NoF",length(fmData$NoF))),
+  value=c(fmData$NoF)
+)
+
+fmDataBox %>%
+  ggplot( aes(x=name, y=value, fill=name)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6) +
+  geom_jitter(color="black", size=0.4, alpha=0.9) +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("boxplot with jitter for NoF") +
+  xlab("")
 
 
