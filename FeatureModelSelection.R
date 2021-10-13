640aa18c5624025289682a79488fcee85b6dd58f
Roberto E. Lopez-Herrejon <roberto.lopez@etsmtl.ca>
2021-10-13 11:54:18 -0400

    First code

--- FeatureModelSelection.R
+++ FeatureModelSelection.R
@@ -1,6 +1,13 @@
 # Feature Model Selection
+# Objective: Randomnly selects FMs 
 # Project: Eye-Tracking Analysis of Feature Models Comprehension
 # Ecole de technologie superieure
 # VERITAS team
-# Authors
-# 
+# Authors: Elmira Sepasi, Kambiz Belouchi, Roberto E. Lopez-Herrejon
+
+
+# This code is written to select FM comprehension's experiment samples.
+
+fmData <- read.csv(file = "fmstats.csv", header=TRUE)
+attach (fmData)
+fmData
