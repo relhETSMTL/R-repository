da506e87d6e170cd7e59f9e01c22135e02f44ac0
Roberto E. Lopez-Herrejon <roberto.lopez@etsmtl.ca>
2021-11-09 23:30:55 -0500

    Pushing commits

--- ExperimentConfigurationGenerator.R
+++ ExperimentConfigurationGenerator.R
@@ -62,7 +62,8 @@ configuration.randomized <- "randomized // either randomized or the sequence of 
 set.seed(10) # seeds the random number generator,
 for(i in 1:number.configurations) {
   
-  # Obtains a random sample of all the questions
+  # Obtains a random sample of all the questions, without considering the warm up questions
+  # which always go in the same order
   samplei <- sample(1:nrow(questionsData), nrow(questionsData))
   
   # Creates a new dataframe with the same structure but with a different order of the questions
