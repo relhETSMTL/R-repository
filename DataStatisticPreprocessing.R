465fcc58d3b295273e0765a9fc41b3e3439d505a
Roberto E. Lopez-Herrejon <roberto.lopez@etsmtl.ca>
2021-12-04 11:15:37 -0500

    Correct addition of NoF, NoC, CombNum columns in the data.

--- DataStatisticPreprocessing.R
+++ DataStatisticPreprocessing.R
@@ -22,26 +22,195 @@ participantData <- participantData %>% filter(QuestionNumer > 0)
 # Adds the column of the participant ID 24 times, one for each question
 participantData$ParticipantID <- rep(2,24)
 
-# Adds dummy values for NoC and NOF columns
+# Adds dummy values for NoC and NOF columns, nd combination number
 participantData$NoF <- rep(1,24)
 participantData$NoC <- rep(1,24)
+participantData$CombNum <- rep(1,24) 
 
-# Readjusts the positions
+# Readjusts the positions of the columns for readibilty
 participantData <- participantData %>% relocate(NoF, .before = Correct)
 participantData <- participantData %>% relocate(NoC, .before = Correct)
 participantData <- participantData %>% relocate(ParticipantID, .before = Correct)
+participantData <- participantData %>% relocate(CombNum, .before = ParticipantID)
 
-# Todo, substitute the values of NoF and NoC according to the question number
+# Traverses the 24 questions of the data frame adjusting the  NOF, NoC, and CombNum
+for (question in 1:24) {
+ 
+  # Question 1
+  if(participantData[question,]$QuestionNumer==1) {
+    participantData[question,]$NoF <- 1
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  
+  # Question 2
+  if(participantData[question,]$QuestionNumer==2) {
+    participantData[question,]$NoF <- 1
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 3
+  if(participantData[question,]$QuestionNumer==3) {
+    participantData[question,]$NoF <- 1
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 4
+  if(participantData[question,]$QuestionNumer==4) {
+    participantData[question,]$NoF <- 1
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 5
+  if(participantData[question,]$QuestionNumer==5) {
+    participantData[question,]$NoF <- 1
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 6
+  if(participantData[question,]$QuestionNumer==6) {
+    participantData[question,]$NoF <- 1
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  ###############
+  
+  # Question 7
+  if(participantData[question,]$QuestionNumer==7) {
+    participantData[question,]$NoF <- 2
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  
+  # Question 8
+  if(participantData[question,]$QuestionNumer==8) {
+    participantData[question,]$NoF <- 2
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 9
+  if(participantData[question,]$QuestionNumer==9) {
+    participantData[question,]$NoF <- 2
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 10
+  if(participantData[question,]$QuestionNumer==10) {
+    participantData[question,]$NoF <- 2
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 11
+  if(participantData[question,]$QuestionNumer==11) {
+    participantData[question,]$NoF <- 2
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 12
+  if(participantData[question,]$QuestionNumer==12) {
+    participantData[question,]$NoF <- 2
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  ##############
+  
+  # Question 13
+  if(participantData[question,]$QuestionNumer==13) {
+    participantData[question,]$NoF <- 3
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 14
+  if(participantData[question,]$QuestionNumer==14) {
+    participantData[question,]$NoF <- 3
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 15
+  if(participantData[question,]$QuestionNumer==15) {
+    participantData[question,]$NoF <- 3
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 16
+  if(participantData[question,]$QuestionNumer==16) {
+    participantData[question,]$NoF <- 3
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 17
+  if(participantData[question,]$QuestionNumer==17) {
+    participantData[question,]$NoF <- 3
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 18
+  if(participantData[question,]$QuestionNumer==18) {
+    participantData[question,]$NoF <- 3
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 2
+  }
+  
+  ##############
+  
+  # Question 19
+  if(participantData[question,]$QuestionNumer==19) {
+    participantData[question,]$NoF <- 4
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 1
+  } 
+    
+  # Question 20
+  if(participantData[question,]$QuestionNumer==20) {
+    participantData[question,]$NoF <- 4
+    participantData[question,]$NoC <- 1
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 21
+  if(participantData[question,]$QuestionNumer==21) {
+    participantData[question,]$NoF <- 4
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 22
+  if(participantData[question,]$QuestionNumer==22) {
+    participantData[question,]$NoF <- 4
+    participantData[question,]$NoC <- 2
+    participantData[question,]$CombNum <- 2
+  } 
+  
+  # Question 23
+  if(participantData[question,]$QuestionNumer==23) {
+    participantData[question,]$NoF <- 4
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 1
+  } 
+  
+  # Question 24
+  if(participantData[question,]$QuestionNumer==24) {
+    participantData[question,]$NoF <- 4
+    participantData[question,]$NoC <- 3
+    participantData[question,]$CombNum <- 2
+  }
+  
+} # end of 24 loop for questions
 
-# for (question in 1:24) { print(question) }
-
-# x <- 5
-# if(x > 0){
-#  print("Positive number")
-# }
-
-
-# For this study, we focus on two metrics: 
-# NoF: Number of Features, NoC: number of cross-tree constraints
-# We select the two metrics and the file name and the feature model name
-# ExpData <- fmData %>% select(FileName, FeatureModel, NoF, NoC)
