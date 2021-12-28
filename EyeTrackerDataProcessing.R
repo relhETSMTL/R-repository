36c4b50485029990801623134441a303f60f159e
Roberto E. Lopez-Herrejon <roberto.lopez@etsmtl.ca>
2021-12-28 13:14:02 -0500

    Data for AOIs computed: Window, Question, Answer, Legend

--- EyeTrackerDataProcessing.R
+++ EyeTrackerDataProcessing.R
@@ -35,17 +35,29 @@ attach (participant03)
 # [1] 61
 #> sum(participant03$AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Window.) --> all window
 #[1] 3888
+#         AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Answer.,AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Window.)
+#         AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Window.:AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Answer.)
+
+
+
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+
 
 # Notes: 
 # 1. Eliminating the NA rows dont help as 
 # %>% select(-Column1,-Column2, -Column3, -Column4) %>% drop_na()  
 
+# 2. Removing duplicates, where all the selected values are equal
+# distinct()
+
+#  filter(Sensor=="Eye Tracker") %>% 
 
 curated03 <- participant03 %>% 
-  filter(Eye.movement.type=="Fixation" & Sensor=="Eye Tracker") %>% 
-  select(Eye.movement.type, Eye.movement.type.index, Gaze.event.duration..ms., Sensor, starts_with("AOI.hit"))
-#         AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Answer.,AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Window.)
-#         AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Window.:AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Answer.)
+ filter(Eye.movement.type=="Fixation" & Sensor=="Eye Tracker") %>% 
+  select(Eye.movement.type, Eye.movement.type.index, Gaze.event.duration..ms., Sensor, starts_with("AOI.hit")) %>%
+  distinct()
 
 # What info is repeated?, 
 repeated <- participant03 %>% 
@@ -55,8 +67,40 @@ repeated <- participant03 %>%
 
 
 
+##########################################################################################################
+
 ### Question 20 processing
 question20 <- curated03
 
-numFixations <- nrow(question20)
-totalTime <- sum(question20$Gaze.event.duration..ms.)
+# Totals
+totalFixations <- nrow(question20)
+totalFixationTime <- sum(question20$Gaze.event.duration..ms.)
+
+# AOI Window
+fixations.Window <- sum(question20$AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Window.)
+perc.fixations.Window <- fixations.Window / numFixations
+
+# AOI Question
+question20.Question <- question20 %>% filter(AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Question.=="1")
+fixations.Question <- sum(question20$AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Question.)
+perc.fixations.Question <- fixations.Question / totalFixations
+time.Question <- sum(question20.Question$Gaze.event.duration..ms.)
+perc.time.Question <- time.Question / totalFixationTime
+
+# AOI Answer
+question20.Answer <- question20 %>% filter(AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Answer.=="1")
+fixations.Answer <- sum(question20$AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Answer.)
+perc.fixations.Answer <- fixations.Answer / totalFixations
+time.Answer <- sum(question20.Answer$Gaze.event.duration..ms.)
+perc.time.Answer <- time.Answer / totalFixationTime
+
+# AOI Legend
+question20.Legend <- question20 %>% filter(AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Legend.=="1")
+fixations.Legend <- sum(question20$AOI.hit..P03.TOI.Q01.Act20.Snap...Q20.Legend.)
+perc.fixations.Legend <- fixations.Legend / totalFixations
+time.Legend <- sum(question20.Legend$Gaze.event.duration..ms.)
+perc.time.Legend <- time.Legend / totalFixationTime
+
+
+
+
