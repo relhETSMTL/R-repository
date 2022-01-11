f4690c62004f1ede27720cd2faeaaa0c8569b660
Roberto E. Lopez-Herrejon <roberto.lopez@etsmtl.ca>
2022-01-11 11:04:34 -0500

    Reordering of Java interface data. Adding eye-tracker data P14, P17, P18, P19

--- CompilingParticipantsData.R
+++ CompilingParticipantsData.R
@@ -1,4 +1,4 @@
-# Reads the computed data for the 17 participants, put them together in a single data frame, and joins the data
+# Reads the computed data for the 18 participants, put them together in a single data frame, and joins the data
 # with the Java interface data
 
 library(ggplot2)
@@ -24,10 +24,12 @@ p07 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP07/
 attach(p07)
 p08 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP08/P08.csv", header=TRUE)
 attach(p08)
-p09 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP09/P09.csv", header=TRUE)
-attach(p09)
-p10 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP10/P10.csv", header=TRUE)
-attach(p10)
+# pending
+# p09 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP09/P09.csv", header=TRUE)
+# attach(p09)
+# pending
+# p10 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP10/P10.csv", header=TRUE)
+# attach(p10)
 p11 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP11/P11.csv", header=TRUE)
 attach(p11)
 p12 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP12/P12.csv", header=TRUE)
@@ -36,17 +38,23 @@ p13 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP13/
 attach(p13)
 p14 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP14/P14.csv", header=TRUE)
 attach(p14)
-p15 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP15/P15.csv", header=TRUE)
-attach(p15)
-p16 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP16/P16.csv", header=TRUE)
-attach(p16)
+# participant with faulty data
+# p15 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP15/P15.csv", header=TRUE)
+# attach(p15)
+#pending
+# p16 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP16/P16.csv", header=TRUE)
+# attach(p16)
 p17 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP17/P17.csv", header=TRUE)
 attach(p17)
 p18 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP18/P18.csv", header=TRUE)
 attach(p18)
+p19 <- read.csv(file = "../../Experiment-Data/Eye-tracking-data-samples/PartP19/P19.csv", header=TRUE)
+attach(p19)
+
 
 # Puts alls the data of the eye tracker into a single data frame
-allEyeTrackerData <- rbind(p02,p03,p04,p05,p06,p07,p08,p09,p10,p11,p12,p13,p14,p15,p16,p17,p18)
+# pending p09,p10, p16,
+allEyeTrackerData <- rbind(p02,p03,p04,p05,p06,p07,p08,p11,p12,p13,p14,p17,p18,p19)
 
 # Stores all the eye-tracker data into a file
 write.csv(allEyeTrackerData,file = "../../Experiment-Data/Eye-tracking-data-samples/EyeTrackerData.csv", row.names = FALSE)
--- DataStatisticPreprocessing.R
+++ DataStatisticPreprocessing.R
@@ -3,226 +3,11 @@
 # Ecole de technologie superieure
 # VERITAS team
 # Authors: Elmira Sepasi, Kambiz Belouchi, Roberto E. Lopez-Herrejon
-# Last update: 2021-12-05
+# Last update: 2022-01-11
 
 library(ggplot2)
 library(tidyverse)
 library(hrbrthemes)
-
-# File reads the experiment data 
-participantData <- read.csv(file = "../../Experiment-Data/Participant-10.csv", header=TRUE)
-attach (participantData)
-
-# Filters the Question number, Correct, and Elapsed Time
-participantData <- participantData %>% select(QuestionNumer, Correct, ElapsedTime)
-
-# Removes the first two rows
-participantData <- participantData %>% filter(QuestionNumer > 0) 
-
-# Adds the column of the participant ID 24 times, one for each question
-participantData$ParticipantID <- rep(2,24)
-
-# Adds dummy values for NoC and NOF columns, nd combination number
-participantData$NoF <- rep(1,24)
-participantData$NoC <- rep(1,24)
-participantData$CombNum <- rep(1,24) 
-
-# Readjusts the positions of the columns for readibilty
-participantData <- participantData %>% relocate(NoF, .before = Correct)
-participantData <- participantData %>% relocate(NoC, .before = Correct)
-participantData <- participantData %>% relocate(ParticipantID, .before = Correct)
-participantData <- participantData %>% relocate(CombNum, .before = ParticipantID)
-
-# Traverses the 24 questions of the data frame adjusting the  NOF, NoC, and CombNum
-for (question in 1:24) {
- 
-  # Question 1
-  if(participantData[question,]$QuestionNumer==1) {
-    participantData[question,]$NoF <- 1
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  
-  # Question 2
-  if(participantData[question,]$QuestionNumer==2) {
-    participantData[question,]$NoF <- 1
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 3
-  if(participantData[question,]$QuestionNumer==3) {
-    participantData[question,]$NoF <- 1
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 4
-  if(participantData[question,]$QuestionNumer==4) {
-    participantData[question,]$NoF <- 1
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 5
-  if(participantData[question,]$QuestionNumer==5) {
-    participantData[question,]$NoF <- 1
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 6
-  if(participantData[question,]$QuestionNumer==6) {
-    participantData[question,]$NoF <- 1
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  ###############
-  
-  # Question 7
-  if(participantData[question,]$QuestionNumer==7) {
-    participantData[question,]$NoF <- 2
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  
-  # Question 8
-  if(participantData[question,]$QuestionNumer==8) {
-    participantData[question,]$NoF <- 2
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 9
-  if(participantData[question,]$QuestionNumer==9) {
-    participantData[question,]$NoF <- 2
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 10
-  if(participantData[question,]$QuestionNumer==10) {
-    participantData[question,]$NoF <- 2
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 11
-  if(participantData[question,]$QuestionNumer==11) {
-    participantData[question,]$NoF <- 2
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 12
-  if(participantData[question,]$QuestionNumer==12) {
-    participantData[question,]$NoF <- 2
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  ##############
-  
-  # Question 13
-  if(participantData[question,]$QuestionNumer==13) {
-    participantData[question,]$NoF <- 3
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 14
-  if(participantData[question,]$QuestionNumer==14) {
-    participantData[question,]$NoF <- 3
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 15
-  if(participantData[question,]$QuestionNumer==15) {
-    participantData[question,]$NoF <- 3
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 16
-  if(participantData[question,]$QuestionNumer==16) {
-    participantData[question,]$NoF <- 3
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 17
-  if(participantData[question,]$QuestionNumer==17) {
-    participantData[question,]$NoF <- 3
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 18
-  if(participantData[question,]$QuestionNumer==18) {
-    participantData[question,]$NoF <- 3
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 2
-  }
-  
-  ##############
-  
-  # Question 19
-  if(participantData[question,]$QuestionNumer==19) {
-    participantData[question,]$NoF <- 4
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 1
-  } 
-    
-  # Question 20
-  if(participantData[question,]$QuestionNumer==20) {
-    participantData[question,]$NoF <- 4
-    participantData[question,]$NoC <- 1
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 21
-  if(participantData[question,]$QuestionNumer==21) {
-    participantData[question,]$NoF <- 4
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 22
-  if(participantData[question,]$QuestionNumer==22) {
-    participantData[question,]$NoF <- 4
-    participantData[question,]$NoC <- 2
-    participantData[question,]$CombNum <- 2
-  } 
-  
-  # Question 23
-  if(participantData[question,]$QuestionNumer==23) {
-    participantData[question,]$NoF <- 4
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 1
-  } 
-  
-  # Question 24
-  if(participantData[question,]$QuestionNumer==24) {
-    participantData[question,]$NoF <- 4
-    participantData[question,]$NoC <- 3
-    participantData[question,]$CombNum <- 2
-  }
-  
-} # end of 24 loop for questions
-
-# TODO
-# Change the column name to QNumber
-participantData <- participantData %>% rename(QNumber=QuestionNumer)
-
-
-# Save to a diffent output file  Participant-1-Curated-Data
-# Writes the sample in a experiment-sample.csv file. Adjust path accordingly.
-write.csv(participantData,"../../Experiment-Data/Participant-2-Curated-Data.csv", row.names = FALSE)
-
 
 ############################################################################################
 # Make it into a function to call with arguments: ParticipantNumber, InputFile, OutputFile
@@ -464,10 +249,13 @@ transformParticipantData(11,"../../Experiment-Data/Participant-11.csv", "../../E
 transformParticipantData(12,"../../Experiment-Data/Participant-12.csv", "../../Experiment-Data/Participant-12-Curated-Data.csv")
 transformParticipantData(13,"../../Experiment-Data/Participant-13.csv", "../../Experiment-Data/Participant-13-Curated-Data.csv")
 transformParticipantData(14,"../../Experiment-Data/Participant-14.csv", "../../Experiment-Data/Participant-14-Curated-Data.csv")
-transformParticipantData(15,"../../Experiment-Data/Participant-15.csv", "../../Experiment-Data/Participant-15-Curated-Data.csv")
 transformParticipantData(16,"../../Experiment-Data/Participant-16.csv", "../../Experiment-Data/Participant-16-Curated-Data.csv")
 transformParticipantData(17,"../../Experiment-Data/Participant-17.csv", "../../Experiment-Data/Participant-17-Curated-Data.csv")
 transformParticipantData(18,"../../Experiment-Data/Participant-18.csv", "../../Experiment-Data/Participant-18-Curated-Data.csv")
+transformParticipantData(19,"../../Experiment-Data/Participant-19.csv", "../../Experiment-Data/Participant-19-Curated-Data.csv")
+
+# faulty student ID data
+# transformParticipantData(15,"../../Experiment-Data/Participant-15.csv", "../../Experiment-Data/Participant-15-Curated-Data.csv")
 
 
 #############################################################################################
@@ -498,17 +286,19 @@ p13 <- read.csv(file = "../../Experiment-Data/Participant-13-Curated-Data.csv", 
 attach (p13)
 p14 <- read.csv(file = "../../Experiment-Data/Participant-14-Curated-Data.csv", header=TRUE)
 attach (p14)
-p15 <- read.csv(file = "../../Experiment-Data/Participant-15-Curated-Data.csv", header=TRUE)
-attach (p15)
+# p15 <- read.csv(file = "../../Experiment-Data/Participant-15-Curated-Data.csv", header=TRUE)
+# attach (p15)
 p16 <- read.csv(file = "../../Experiment-Data/Participant-16-Curated-Data.csv", header=TRUE)
 attach (p16)
 p17 <- read.csv(file = "../../Experiment-Data/Participant-17-Curated-Data.csv", header=TRUE)
 attach (p17)
 p18 <- read.csv(file = "../../Experiment-Data/Participant-18-Curated-Data.csv", header=TRUE)
 attach (p18)
+p19 <- read.csv(file = "../../Experiment-Data/Participant-19-Curated-Data.csv", header=TRUE)
+attach (p19)
 
 # All data samples merged
-allParticipants <- rbind(p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18)
+allParticipants <- rbind(p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p16,p17,p18,p19)
 
 # Sort according to QNumber and ParticipantID
 allParticipants <- allParticipants[
@@ -540,6 +330,222 @@ correctAnswers <- nrow(allParticipants %>% filter(Correct==2))  #284   --> True
 # Save to output file Experiment-All-Participants-Curated-Data
 write.csv(allParticipants, "../../Experiment-Data/All-Participants-Curated-Data.csv", row.names = FALSE)
 
+
+
+# # File reads the experiment data 
+# participantData <- read.csv(file = "../../Experiment-Data/Participant-10.csv", header=TRUE)
+# attach (participantData)
+# 
+# 
 # Verify the errors in responses: "I dont know" --> should be recorded as false. 
 # Only one instance. Participant 15, question 24.
 
+# # Filters the Question number, Correct, and Elapsed Time
+# participantData <- participantData %>% select(QuestionNumer, Correct, ElapsedTime)
+# 
+# # Removes the first two rows
+# participantData <- participantData %>% filter(QuestionNumer > 0) 
+# 
+# # Adds the column of the participant ID 24 times, one for each question
+# participantData$ParticipantID <- rep(2,24)
+# 
+# # Adds dummy values for NoC and NOF columns, nd combination number
+# participantData$NoF <- rep(1,24)
+# participantData$NoC <- rep(1,24)
+# participantData$CombNum <- rep(1,24) 
+
+# # Readjusts the positions of the columns for readibilty
+# participantData <- participantData %>% relocate(NoF, .before = Correct)
+# participantData <- participantData %>% relocate(NoC, .before = Correct)
+# participantData <- participantData %>% relocate(ParticipantID, .before = Correct)
+# participantData <- participantData %>% relocate(CombNum, .before = ParticipantID)
+# 
+# # Traverses the 24 questions of the data frame adjusting the  NOF, NoC, and CombNum
+# for (question in 1:24) {
+#  
+#   # Question 1
+#   if(participantData[question,]$QuestionNumer==1) {
+#     participantData[question,]$NoF <- 1
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   
+#   # Question 2
+#   if(participantData[question,]$QuestionNumer==2) {
+#     participantData[question,]$NoF <- 1
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 3
+#   if(participantData[question,]$QuestionNumer==3) {
+#     participantData[question,]$NoF <- 1
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 4
+#   if(participantData[question,]$QuestionNumer==4) {
+#     participantData[question,]$NoF <- 1
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 5
+#   if(participantData[question,]$QuestionNumer==5) {
+#     participantData[question,]$NoF <- 1
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 6
+#   if(participantData[question,]$QuestionNumer==6) {
+#     participantData[question,]$NoF <- 1
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   ###############
+#   
+#   # Question 7
+#   if(participantData[question,]$QuestionNumer==7) {
+#     participantData[question,]$NoF <- 2
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   
+#   # Question 8
+#   if(participantData[question,]$QuestionNumer==8) {
+#     participantData[question,]$NoF <- 2
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 9
+#   if(participantData[question,]$QuestionNumer==9) {
+#     participantData[question,]$NoF <- 2
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 10
+#   if(participantData[question,]$QuestionNumer==10) {
+#     participantData[question,]$NoF <- 2
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 11
+#   if(participantData[question,]$QuestionNumer==11) {
+#     participantData[question,]$NoF <- 2
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 12
+#   if(participantData[question,]$QuestionNumer==12) {
+#     participantData[question,]$NoF <- 2
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   ##############
+#   
+#   # Question 13
+#   if(participantData[question,]$QuestionNumer==13) {
+#     participantData[question,]$NoF <- 3
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 14
+#   if(participantData[question,]$QuestionNumer==14) {
+#     participantData[question,]$NoF <- 3
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 15
+#   if(participantData[question,]$QuestionNumer==15) {
+#     participantData[question,]$NoF <- 3
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 16
+#   if(participantData[question,]$QuestionNumer==16) {
+#     participantData[question,]$NoF <- 3
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 17
+#   if(participantData[question,]$QuestionNumer==17) {
+#     participantData[question,]$NoF <- 3
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 18
+#   if(participantData[question,]$QuestionNumer==18) {
+#     participantData[question,]$NoF <- 3
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 2
+#   }
+#   
+#   ##############
+#   
+#   # Question 19
+#   if(participantData[question,]$QuestionNumer==19) {
+#     participantData[question,]$NoF <- 4
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 1
+#   } 
+#     
+#   # Question 20
+#   if(participantData[question,]$QuestionNumer==20) {
+#     participantData[question,]$NoF <- 4
+#     participantData[question,]$NoC <- 1
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 21
+#   if(participantData[question,]$QuestionNumer==21) {
+#     participantData[question,]$NoF <- 4
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 22
+#   if(participantData[question,]$QuestionNumer==22) {
+#     participantData[question,]$NoF <- 4
+#     participantData[question,]$NoC <- 2
+#     participantData[question,]$CombNum <- 2
+#   } 
+#   
+#   # Question 23
+#   if(participantData[question,]$QuestionNumer==23) {
+#     participantData[question,]$NoF <- 4
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 1
+#   } 
+#   
+#   # Question 24
+#   if(participantData[question,]$QuestionNumer==24) {
+#     participantData[question,]$NoF <- 4
+#     participantData[question,]$NoC <- 3
+#     participantData[question,]$CombNum <- 2
+#   }
+#   
+# } # end of 24 loop for questions
+# 
+# # TODO
+# # Change the column name to QNumber
+# participantData <- participantData %>% rename(QNumber=QuestionNumer)
+# 
+# 
+# # Save to a diffent output file  Participant-1-Curated-Data
+# # Writes the sample in a experiment-sample.csv file. Adjust path accordingly.
+# write.csv(participantData,"../../Experiment-Data/Participant-2-Curated-Data.csv", row.names = FALSE)
