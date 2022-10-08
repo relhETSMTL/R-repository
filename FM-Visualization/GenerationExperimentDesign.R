3ecb0dfba1a7475c6993f1581d5b9352f22aa844
relhETSMTL <roberto.lopez@etsmtl.ca>
2022-10-08 09:01:37 -0400

    Complete generation done. Needs to be tested.

--- FM-Visualization/GenerationExperimentDesign.R
+++ FM-Visualization/GenerationExperimentDesign.R
@@ -36,7 +36,7 @@ choices <- c(1:4) # 1,2,3,4  different value assignments
 coverageAssignmentsOptions <- permutations(n = 4, r = 4, v = choices, repeats.allowed = FALSE)
 
 
-# CReating data frame to hold the experiment data matrix
+# Creating data frame to hold the experiment data matrix
 # QN - question number, T  strength 2 or 3, Vis visualization 1 or 2
 # FM feature model name
 # Y-N pair/triplet the expected correct answer is covered (True) or not (False) 
@@ -44,6 +44,31 @@ column.names <- c("QN", "T", "Vis", "FM", "Y-N")
 
 experimentDesign.data.frame <- 
   setNames(data.frame(matrix(ncol = 5, nrow = 0)), column.names)
+
+
+# Function that maps the coverage permutations to boolean values
+# Mapping 1=true, 2=false, 3=true, 4=false
+
+mapCoverageToBoolean = function (sampledCoverage) {
+
+  # if equal to 1
+  if (sampledCoverage == 1) result <- TRUE
+  
+  
+  # if equal to 2
+  if (sampledCoverage == 2) result <- FALSE 
+  
+  # if equal to 3
+  if (sampledCoverage == 3) result <- TRUE
+
+  
+  # if equal to 4 
+  if (sampledCoverage == 4)  result <- FALSE
+
+  return(result)
+  
+}  # end of function mapCoverageToBoolean
+
 
 
 ##################################
@@ -63,22 +88,22 @@ experimentDesign.data.frame[1,1] ="1"
 experimentDesign.data.frame[1,2] ="2"
 experimentDesign.data.frame[1,3] ="1"
 experimentDesign.data.frame[1,4] =as.vector(q1FM$Q1[1])
-experimentDesign.data.frame[1,5] =coverage$V1[1]
+experimentDesign.data.frame[1,5] =mapCoverageToBoolean(sampledCoverage[1])
 
 # Question 2
 experimentDesign.data.frame[2,1] ="2"
 experimentDesign.data.frame[2,2] ="2"
 experimentDesign.data.frame[2,3] ="1"
 experimentDesign.data.frame[2,4] =as.vector(q1FM$Q1[2])
-experimentDesign.data.frame[2,5] =coverage$V2[1]
+experimentDesign.data.frame[2,5] =mapCoverageToBoolean(sampledCoverage[2])
 
 
 # Question 3
 experimentDesign.data.frame[3,1] ="3"
 experimentDesign.data.frame[3,2] ="2"
 experimentDesign.data.frame[3,3] ="1"
-experimentDesign.data.frame[3,4] = as.vector(q2FM$Q2[1])
-experimentDesign.data.frame[3,5] =coverage$V3[1]
+experimentDesign.data.frame[3,4] =as.vector(q2FM$Q2[1])
+experimentDesign.data.frame[3,5] =mapCoverageToBoolean(sampledCoverage[3])
 
 
 # Question 4
@@ -86,7 +111,7 @@ experimentDesign.data.frame[4,1] ="4"
 experimentDesign.data.frame[4,2] ="2"
 experimentDesign.data.frame[4,3] ="1"
 experimentDesign.data.frame[4,4] =as.vector(q2FM$Q2[2])
-experimentDesign.data.frame[4,5] =coverage$V4[1]
+experimentDesign.data.frame[4,5] =mapCoverageToBoolean(sampledCoverage[4])
 
 
 ##################################
@@ -106,14 +131,14 @@ experimentDesign.data.frame[5,1] ="5"
 experimentDesign.data.frame[5,2] ="2"
 experimentDesign.data.frame[5,3] ="2"
 experimentDesign.data.frame[5,4] =as.vector(q1FM$Q1[1])
-experimentDesign.data.frame[5,5] =coverage$V1[1]
+experimentDesign.data.frame[5,5] =mapCoverageToBoolean(sampledCoverage[1])
 
 # Question 6
 experimentDesign.data.frame[6,1] ="6"
 experimentDesign.data.frame[6,2] ="2"
 experimentDesign.data.frame[6,3] ="2"
 experimentDesign.data.frame[6,4] =as.vector(q1FM$Q1[2])
-experimentDesign.data.frame[6,5] =coverage$V2[1]
+experimentDesign.data.frame[6,5] =mapCoverageToBoolean(sampledCoverage[2])
 
 
 # Question 7
@@ -121,7 +146,7 @@ experimentDesign.data.frame[7,1] ="7"
 experimentDesign.data.frame[7,2] ="2"
 experimentDesign.data.frame[7,3] ="2"
 experimentDesign.data.frame[7,4] =as.vector(q2FM$Q2[1])
-experimentDesign.data.frame[7,5] =coverage$V3[1]
+experimentDesign.data.frame[7,5] =mapCoverageToBoolean(sampledCoverage[3])
 
 
 # Question 8
@@ -129,7 +154,7 @@ experimentDesign.data.frame[8,1] ="8"
 experimentDesign.data.frame[8,2] ="2"
 experimentDesign.data.frame[8,3] ="2"
 experimentDesign.data.frame[8,4] =as.vector(q2FM$Q2[2])
-experimentDesign.data.frame[8,5] =coverage$V4[1]
+experimentDesign.data.frame[8,5] =mapCoverageToBoolean(sampledCoverage[4])
 
 
 ##################################
@@ -148,14 +173,14 @@ experimentDesign.data.frame[9,1] ="9"
 experimentDesign.data.frame[9,2] ="3"
 experimentDesign.data.frame[9,3] ="1"
 experimentDesign.data.frame[9,4] =as.vector(q1FM$Q1[1])
-experimentDesign.data.frame[9,5] =coverage$V1[1]
+experimentDesign.data.frame[9,5] =mapCoverageToBoolean(sampledCoverage[1])
 
 # Question 10
 experimentDesign.data.frame[10,1] ="10"
 experimentDesign.data.frame[10,2] ="3"
 experimentDesign.data.frame[10,3] ="1"
 experimentDesign.data.frame[10,4] =as.vector(q1FM$Q1[2])
-experimentDesign.data.frame[10,5] =coverage$V2[1]
+experimentDesign.data.frame[10,5] =mapCoverageToBoolean(sampledCoverage[2])
 
 
 # Question 11
@@ -163,7 +188,7 @@ experimentDesign.data.frame[11,1] ="11"
 experimentDesign.data.frame[11,2] ="3"
 experimentDesign.data.frame[11,3] ="1"
 experimentDesign.data.frame[11,4] =as.vector(q2FM$Q2[1])
-experimentDesign.data.frame[11,5] =coverage$V3[1]
+experimentDesign.data.frame[11,5] =mapCoverageToBoolean(sampledCoverage[3])
 
 
 # Question 12
@@ -171,7 +196,7 @@ experimentDesign.data.frame[12,1] ="12"
 experimentDesign.data.frame[12,2] ="3"
 experimentDesign.data.frame[12,3] ="1"
 experimentDesign.data.frame[12,4] =as.vector(q2FM$Q2[2])
-experimentDesign.data.frame[12,5] =coverage$V4[1]
+experimentDesign.data.frame[12,5] =mapCoverageToBoolean(sampledCoverage[4])
 
 
 ##################################
@@ -190,14 +215,14 @@ experimentDesign.data.frame[13,1] ="13"
 experimentDesign.data.frame[13,2] ="3"
 experimentDesign.data.frame[13,3] ="2"
 experimentDesign.data.frame[13,4] =as.vector(q1FM$Q1[1])
-experimentDesign.data.frame[13,5] =coverage$V1[1]
+experimentDesign.data.frame[13,5] =mapCoverageToBoolean(sampledCoverage[1])
 
 # Question 14
 experimentDesign.data.frame[14,1] ="14"
 experimentDesign.data.frame[14,2] ="3"
 experimentDesign.data.frame[14,3] ="2"
 experimentDesign.data.frame[14,4] =as.vector(q1FM$Q1[2])
-experimentDesign.data.frame[14,5] =coverage$V2[1]
+experimentDesign.data.frame[14,5] =mapCoverageToBoolean(sampledCoverage[2])
 
 
 # Question 15
@@ -205,7 +230,7 @@ experimentDesign.data.frame[15,1] ="15"
 experimentDesign.data.frame[15,2] ="3"
 experimentDesign.data.frame[15,3] ="2"
 experimentDesign.data.frame[15,4] =as.vector(q2FM$Q2[1])
-experimentDesign.data.frame[15,5] =coverage$V3[1]
+experimentDesign.data.frame[15,5] =mapCoverageToBoolean(sampledCoverage[3])
 
 
 # Question 16
@@ -213,15 +238,13 @@ experimentDesign.data.frame[16,1] ="16"
 experimentDesign.data.frame[16,2] ="3"
 experimentDesign.data.frame[16,3] ="2"
 experimentDesign.data.frame[16,4] =as.vector(q2FM$Q2[2])
-experimentDesign.data.frame[16,5] =coverage$V4[1]
+experimentDesign.data.frame[16,5] =mapCoverageToBoolean(sampledCoverage[4])
 
 
 #####################
 
 # Writes out the experiment design file
 write.csv(experimentDesign.data.frame, "./FM-Visualization/ExperimentDesign.csv", row.names = FALSE, quote=FALSE)
-
-
 
 # Format of the generated file
 # QN	T	Vis	FM	Y-N
@@ -231,19 +254,19 @@ write.csv(experimentDesign.data.frame, "./FM-Visualization/ExperimentDesign.csv"
 # 4	2	1	FM29	Yes
 
 
-## Testing the computation permutations of Yes, No
-
-# R starts with 1, so we need to create a list of 0 to 10
-choices = c(0:9)
-
-# n is the number of options, r is the number of groups, v is the value of our choices
-res = permutations(n = 10, r = 3, v = choices, repeats.allowed = FALSE)
-
-
-## For our purposes
-## 4 x 3 X 2 X 1 = 24 combinations
-choices = c(1:4)
-res = permutations(n = 4, r = 4, v = choices, repeats.allowed = FALSE)
+# ## Testing the computation permutations of Yes, No
+# 
+# # R starts with 1, so we need to create a list of 0 to 10
+# choices = c(0:9)
+# 
+# # n is the number of options, r is the number of groups, v is the value of our choices
+# res = permutations(n = 10, r = 3, v = choices, repeats.allowed = FALSE)
+# 
+# 
+# ## For our purposes
+# ## 4 x 3 X 2 X 1 = 24 combinations
+# choices = c(1:4)
+# res = permutations(n = 4, r = 4, v = choices, repeats.allowed = FALSE)
 
 # Examples of obtaining the permutation data
 # > res[1,]
@@ -254,4 +277,11 @@ res = permutations(n = 4, r = 4, v = choices, repeats.allowed = FALSE)
 # [1] 2
 # > res[21,][3]
 
+# if (sampledCoverage[1] == 3) {
+#   TRUE
+# }
+# 
+# if (sampledCoverage[1] == 4) {
+#   TRUE
+# }
 
