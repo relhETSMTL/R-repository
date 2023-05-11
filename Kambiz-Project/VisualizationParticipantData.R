54ffd0cb93f37e38a43bab55e98a4e9353d602f5
relhETSMTL <roberto.lopez@etsmtl.ca>
2023-05-11 19:06:12 -0400

    Participants data reordered by actual question number and column name problem fixed

--- Kambiz-Project/VisualizationParticipantData.R
+++ Kambiz-Project/VisualizationParticipantData.R
@@ -1,3 +1,17 @@
 # Kambiz project
 # Participants response data
 
+library("tidyverse")
+
+
+# Loads original participants responses
+participant.responses <- read.csv(file = "../../../Eye-Tracking-Visualization/Experiment-Data/ExperimentParticipantsResponses.csv", header=TRUE)
+attach(participant.responses)
+
+
+# Sorts the data frame by the ActualQuestionNumber, ranging from 1..16
+participants.by.actualquestion <- participant.responses %>% arrange(ActualQuestionNumber)
+
+# Fixes the column name problem of ParticipantsQuestionpNumber to ParticipantsQuestionNumber
+participants.by.actualquestion <- participants.by.actualquestion %>% rename("ParticipantsQuestionNumber" = "ParticipantsQuestiopnNumber")
+
