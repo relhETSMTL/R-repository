0d5b592e5f2b7320a82511472df70448d817d680
relhETSMTL <roberto.lopez@etsmtl.ca>
2023-09-27 20:18:33 -0400

    Failed basic consistency checks

--- VISSOFT23-Inferential-Statistics.R
+++ VISSOFT23-Inferential-Statistics.R
@@ -8,3 +8,111 @@ library("tidyverse")
 all.gaze.data <- read.csv(file = "../../../Eye-Tracking-Visualization/Experiment-Data/Analyzed-2023-09-18.csv", 
                           header=TRUE)
 
+
+all.gaze.data[5,]$Question.pfcount + all.gaze.data[5,]$Response.pfcount + 
+   all.gaze.data[5,]$Misc.pfcount + all.gaze.data[5,]$Navigation.pfcount + 
+   all.gaze.data[5,]$Axial.pfcount + all.gaze.data[5,]$Solution.pfcount + all.gaze.data[5,]$Target.pfcount
+
+
+all.gaze.data[3,]$Question.pfcount + all.gaze.data[3,]$Response.pfcount + 
+  all.gaze.data[3,]$Misc.pfcount + all.gaze.data[3,]$Navigation.pfcount + 
+  all.gaze.data[3,]$Axial.pfcount + all.gaze.data[3,]$Solution.pfcount + all.gaze.data[3,]$Target.pfcount
+
+
+#######################################################################
+#######################################################################
+#######################################################################
+# Donought charts examples
+# load library
+library(ggplot2)
+
+# Create test data.
+data <- data.frame(
+  category=c("A", "B", "C"),
+  count=c(10, 60, 30)
+)
+
+# Compute percentages
+data$fraction = data$count / sum(data$count)
+
+# Compute the cumulative percentages (top of each rectangle)
+data$ymax = cumsum(data$fraction)
+
+# Compute the bottom of each rectangle
+data$ymin = c(0, head(data$ymax, n=-1))
+
+# Make the plot
+ggplot(data, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category)) +
+  geom_rect() +
+  coord_polar(theta="y") + # Try to remove that to understand how the chart is built initially
+  xlim(c(2, 4)) # Try to remove that to see how to make a pie chart
+
+##########
+library(ggplot2)
+
+# Create test data.
+data <- data.frame(
+  category=c("A", "B", "C"),
+  count=c(10, 60, 30)
+)
+
+# Compute percentages
+data$fraction <- data$count / sum(data$count)
+
+# Compute the cumulative percentages (top of each rectangle)
+data$ymax <- cumsum(data$fraction)
+
+# Compute the bottom of each rectangle
+data$ymin <- c(0, head(data$ymax, n=-1))
+
+# Compute label position
+data$labelPosition <- (data$ymax + data$ymin) / 2
+
+# Compute a good label
+data$label <- paste0(data$category, "\n value: ", data$count)
+
+# Make the plot
+ggplot(data, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category)) +
+  geom_rect() +
+  geom_label( x=3.5, aes(y=labelPosition, label=label), size=6) +
+  scale_fill_brewer(palette=4) +
+  coord_polar(theta="y") +
+  xlim(c(2, 4)) +
+  theme_void() +
+  theme(legend.position = "none")
+
+#####
+# load library
+library(ggplot2)
+
+# Create test data.
+data <- data.frame(
+  category=c("A", "B", "C"),
+  count=c(10, 60, 30)
+)
+
+# Compute percentages
+data$fraction <- data$count / sum(data$count)
+
+# Compute the cumulative percentages (top of each rectangle)
+data$ymax <- cumsum(data$fraction)
+
+# Compute the bottom of each rectangle
+data$ymin <- c(0, head(data$ymax, n=-1))
+
+# Compute label position
+data$labelPosition <- (data$ymax + data$ymin) / 2
+
+# Compute a good label
+data$label <- paste0(data$category, "\n value: ", data$count)
+
+# Make the plot
+ggplot(data, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category)) +
+  geom_rect() +
+  geom_text( x=2, aes(y=labelPosition, label=label, color=category), size=6) + # x here controls label position (inner / outer)
+  scale_fill_brewer(palette=3) +
+  scale_color_brewer(palette=3) +
+  coord_polar(theta="y") +
+  xlim(c(-1, 4)) +
+  theme_void() +
+  theme(legend.position = "none")
