aca835244e80bb0c21afc161463a6f76f733a3a8
relhETSMTL <roberto.lopez@etsmtl.ca>
2023-11-13 14:45:20 -0500

    Generation of complete web data file

--- IST-Kambiz-RCode.R
+++ IST-Kambiz-RCode.R
@@ -28,8 +28,42 @@ eye.tracker.data.cleaned <- eye.tracker.data.cleaned %>%
          Visualization.Technique = Visualization,
          Elapsed.Time.Eye.Tracker = ElapsedTime)
 
+# Column names are now
+# "Participant.ID"           "Question.Number"          "T"                        "Visualization.Technique" 
+# "Accuracy"                 "Elapsed.Time.Eye.Tracker" "Fixation.Time"            "Fixation.Count"          
+# "Question.pftime"          "Question.pfcount"         "Response.pftime"          "Response.pfcount"        
+# "Misc.pftime"              "Misc.pfcount"             "Navigation.pftime"        "Navigation.pfcount"      
+# "Axial.pftime"             "Axial.pfcount"            "Solution.pftime"          "Solution.pfcount"        
+# "Target.pftime"            "Target.pfcount"          
+
+# Writes out the cleaned data in a file
+write.csv(eye.tracker.data.cleaned,
+          file = "../../../Eye-Tracking-Visualization/Experiment-Data/Curated-Data/Eye-Tracker-Data-Curated.csv", 
+          row.names=FALSE)
 
 
+###########################################################################
+### Web interface data merging
+t2.web.data <- read.csv(file = "../../../Eye-Tracking-Visualization/Experiment-Data/2-ParticipantsResponses.csv", header=TRUE)
+t3.web.data <- read.csv(file = "../../../Eye-Tracking-Visualization/Experiment-Data/3-ParticipantsResponses.csv", header=TRUE)
+
+complete.web.data <- bind_rows(t2.web.data,t3.web.data)
+
+# Adds column with T values, 8 x 24 participants = 192 per each T value x 2 = 384
+T <- c(rep(2,192),rep(3,192))
+complete.web.data$T = T
 
 
-# Renaming the columns
+# Column names
+# "Participant.ID"       "Question.Number"      "Accuracy"             "Elapsed.Time"         "Certainty.Assessment"
+# "Difficulty.Level"     "Visualization.Method" "Number.Elements"      "T"     
+
+
+# Renaming the column Visualization.Method to Visualization.Technique
+complete.web.data.cleaned <- complete.web.data %>% rename(Visualization.Technique = Visualization.Method)
+
+
+# Writes out the cleaned data in the file
+write.csv(complete.web.data.cleaned,
+          file = "../../../Eye-Tracking-Visualization/Experiment-Data/Curated-Data/Web-Interface-Data-Curated.csv", 
+          row.names=FALSE)
