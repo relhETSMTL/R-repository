eb43b4f691c8870cfee1ed43b3dddf87327a0657
Roberto Lopez <roberto.lopez@etsmtl.ca>
2024-04-15 17:35:08 -0400

    Adding the iteration loops for all participants and questions

--- Elmira-Transitions/ComputeGSEQFormat.R
+++ Elmira-Transitions/ComputeGSEQFormat.R
@@ -7,9 +7,9 @@ library(rstudioapi)
 setwd(dirname(getActiveDocumentContext()$path))
 
 # Loads the entire set of curated transitions
-all.participants.trans <- read.csv(file = "../../../Experiment-Data/All-Participants-Transitions-Curated-Data.csv", 
+all.participants.fixations <- read.csv(file = "../../../Experiment-Data/All-Participants-Transitions-Curated-Data.csv", 
                                    header=TRUE)
-attach (all.participants.trans)
+attach (all.participants.fixations)
 
 # Read the merged data of transitions and information from the questions, NoF, NoC, number of features and type of 
 # configuration
@@ -32,23 +32,90 @@ attach (all.participants.collated)
 
 
 
+# Defines the output configuration file
+file.path <- 'config.txt'
 
+# writes the header
+writeLines("Here goes the header", file.path)
+
+# for all the participants and for all the questions create their characteristics descriptor and 
+# its sequence of aois
+
+participants.ids <-unique(all.participants.fixations$Participant) 
+question.nums <-  unique(all.participants.fixations$QN)
+
+## For all the participants
+for (participant.id in participants.ids) {
+  
+  for (question.num in question.nums) {
+    cat("[",participant.id,"-",question.num,"]") 
+    
+    # contains the fixations data for a participant and a question 
+    participant.question.data <- all.participants.fixations %>% 
+      filter(Participant==participant.id &	QN==question.num)
+    
+    # computes the string with the aois in the sequence
+    aois.sequence <- paste(paste(participant.question.data$IDAOI, collapse = " "),"/",sep="")
+    
+    # obtains the information of the question in place for the given participant
+    response.data <- all.participants.collated %>% filter(ParticipantID==participant.id & QNumber==question.num)
+    
+    # creates the string describing the characteristics of the sequences in terms of its predictors
+    # the format is <P2Q1> (2, Q1, 1, 1,True, 3 ,  Full)
+    QN <- paste("Q",question.num,sep="")
+    NoF <-  response.data[1,"NoF"]
+    NoC <-  response.data[1,"NoC"]
+    Correct <- response.data[1,"Correct"]
+    FeaturesInQuestion <- response.data[1,"FeaturesInQuestion"]
+    ConfigurationType <- response.data[1,"ConfigurationType"]
+    participant.sequence.descriptor <- 
+      toString(c(participant.id,QN,NoF,NoC,Correct,FeaturesInQuestion,ConfigurationType))
+    header <- paste("<P",participant.id,"Q",question.num,"> ",sep="")# <P2Q1>_ with extra white space at the end _
+    sequence.header.descriptor <- paste(header,"(",participant.sequence.descriptor,")",sep="")
+    
+    # writes in the file the sequence descriptor
+    write(sequence.header.descriptor, file.path, append=TRUE) 
+  
+    # write in the file the aois sequence
+    write(aois.sequence, file.path, append=TRUE) 
+    
+  } # of all questions
+  
+} # of all participants
+
+    
+
+
+############################################
+############################################
 
 # Testing searching the data for a participant and a question
-
 
 
 # contains the data for a participant and a question 
 participant.question.data <- all.participants.trans %>% filter(Participant==2 &	QN==1)
 
 # computes the string with the aois in the sequence
-aois.sequence <- paste(participant.question.data$IDAOI, collapse = " ")
+aois.sequence <- paste(paste(participant.question.data$IDAOI, collapse = " "),"/",sep="")
 
 # obtains the information of the question in place for the given participant
 response.data <- all.participants.collated %>% filter(ParticipantID==2 & QNumber==1)
 
+
+
 # creates the string describing the characteristics of the sequences in terms of its predictors
 # the format is <P2Q1> (2, Q1, 1, 1,True, 3 ,  Full)
-participant.sequence.descriptor <- toString(c(2,"Q1",1,1,"True",2,3,"Partial"))
+participant.sequence.descriptor <- toString(c(2,"Q1",1,1,"True",3,"Partial"))
 sequence.header.descriptor <- paste("<P2Q1> (",participant.sequence.descriptor,")",sep="")
 
+outFile <- file("config.txt")
+writeLines(sequence.header.descriptor, outFile)
+writeLines(aois.sequence, outFile)
+close(outFile)
+file.show("config.txt")
+
+
+
+file.path <- 'config.txt'
+writeLines(sequence.header.descriptor, file.path)
+write(aois.sequence, file.path, append=TRUE) 
